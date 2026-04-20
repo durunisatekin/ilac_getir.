d77d40889f01139ce87efc73ba1657266e20f3e9
durunisatekin <durunisatekin@gmail.com>
2026-04-20 17:32:36 +0300

    dıfference

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,17 +1,17 @@
 class CartItem {
   String name;
   double price;
-  int quantity;
+  int quantity; // quantity= adet(miktar)
 
   CartItem({required this.name, required this.price, this.quantity = 1});
-}
+} // sepetteki tek bir ürünü temsil ediyor
 
 class Cart {
-  List<CartItem> items = [];
+  List<CartItem> items = []; // sepetin kendisi içinde bir sürü CartItem var
 
   void addItem(String name, double price) {
     int index = items.indexWhere((item) => item.name == name);
-
+    // sepette bu ürün var mı bakıyor
     if (index != -1) {
       items[index].quantity++;
     } else {
@@ -20,7 +20,7 @@ class Cart {
   }
 
   void increase(CartItem item) {
-    item.quantity++;
+    item.quantity++; // sepetteki + butonu sadece sayıyı arttırır
   }
 
   void decrease(CartItem item) {
@@ -28,14 +28,16 @@ class Cart {
       item.quantity--;
     } else {
       items.remove(item);
-    }
+    } // eğer 1'den büyükse azalt , eğer 1 ise tamamen sil.
   }
 
   double get totalPrice {
     return items.fold(0, (sum, item) => sum + item.price * item.quantity);
-  }
+  } // fold= toplama işlemi
 
   int get totalItems {
     return items.fold(0, (sum, item) => sum + item.quantity);
-  }
+  } // toplam kaç ürün var (adet olaarak )
 }
+
+// default = varsayılan değer
--- lib/screens/ilac_listesi.dart
+++ lib/screens/ilac_listesi.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import '../models/cart_model.dart';
 
-class IlacListesi extends StatelessWidget {
+class IlacListesi extends StatefulWidget {
   final Cart cart;
   final String kategori;
 
-  IlacListesi({required this.cart, required this.kategori});
+  const IlacListesi({super.key, required this.cart, required this.kategori});
+
+  @override
+  State<IlacListesi> createState() => _IlacListesiState();
+}
+
+class _IlacListesiState extends State<IlacListesi> {
+  String search = "";
 
   final List<Map<String, dynamic>> tumIlaclar = [
     {"name": "Parol", "price": 50.0, "type": "agri"},
@@ -32,30 +39,63 @@ class IlacListesi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ilaclar = tumIlaclar.where((i) => i["type"] == kategori).toList();
+    final ilaclar = tumIlaclar.where((i) {
+      return i["type"] == widget.kategori &&
+          i["name"].toLowerCase().contains(search.toLowerCase());
+    }).toList();
 
     return Scaffold(
-      appBar: AppBar(title: Text("İlaçlar")),
-      body: ListView.builder(
-        itemCount: ilaclar.length,
-        itemBuilder: (context, index) {
-          final ilac = ilaclar[index];
-
-          return ListTile(
-            title: Text(ilac["name"]),
-            subtitle: Text("${ilac["price"]} TL"),
-            trailing: ElevatedButton(
-              onPressed: () {
-                cart.addItem(ilac["name"], ilac["price"]);
-
-                ScaffoldMessenger.of(
-                  context,
-                ).showSnackBar(SnackBar(content: Text("Sepete eklendi")));
+      appBar: AppBar(title: const Text("İlaçlar")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: "İlaç ara...",
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  search = value;
+                });
               },
-              child: Text("Sepete Ekle"),
             ),
-          );
-        },
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: ilaclar.length,
+              itemBuilder: (context, index) {
+                final ilac = ilaclar[index];
+
+                return Card(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 5,
+                  ),
+                  child: ListTile(
+                    title: Text(ilac["name"]),
+                    subtitle: Text("${ilac["price"]} TL"),
+                    trailing: ElevatedButton(
+                      onPressed: () {
+                        widget.cart.addItem(ilac["name"], ilac["price"]);
+
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Sepete eklendi"),
+                            duration: Duration(seconds: 1),
+                          ),
+                        );
+                      },
+                      child: const Text("Sepete Ekle"),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
